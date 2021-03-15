aafbd72f6c6d20aeecfda832ea7652bff347c119
HAJIB Diae <diae.hajib@gmail.com>
2021-03-15 12:43:41 +0100

    Add files via upload

--- csharp_s-ance_2/ConsoleApp1/Program.cs
+++ csharp_s-ance_2/ConsoleApp1/Program.cs
@@ -12,12 +12,7 @@ namespace ConsoleApp1
         {
             Client client1 = new Client("diae", "hajib", "test 404");
             Client client2 = new Client("anas", "fartas", "test 369");
-            Compte c1 = new Compte(client1);
-            Compte c2 = new Compte(client2);
 
-            c1.Crediter(new MAD(3000.5));
-            c1.Verser(new MAD(1950.75), c2);
-            c2.Debiter(new MAD(900));
 
             Epargne ep = new Epargne(client1, 1.2);
             ep.Crediter(new MAD(7500.75));
@@ -28,10 +23,7 @@ namespace ConsoleApp1
             cr.Crediter(new MAD(3000));
             cr1.Crediter(new MAD(4000));
             ep.Verser(new MAD(2000),cr );
-            c1.Consulter();
-
-            Console.WriteLine("------------------------------");
-            c2.Consulter();
+            ep.AjouterTaux();
             Console.WriteLine("------------------------------");
             ep.Consulter();
             Console.WriteLine("------------------------------");
